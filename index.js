479e8247df84f11e99feeab4c6db36e1b8ad0d76
rockshang <s2265681@163.com>
2021-12-24 22:59:11 +0800

    feat: 区分白天晚上

--- index.js
+++ index.js
@@ -3,14 +3,8 @@
  * @Autor: rockshang
  * @Date: 2021-12-24 11:59:44
  */
-import { getCurrentTimeInfo } from "./js/utils.js";
 import countDownInit from "./js/countdown.js";
 
 window.onload = async function () {
-
   countDownInit();
-  // weatherCallback.then((res) => {
-  //   console.log("当前天气", res);
-  // });
-  // console.log("当前时间信息", getCurrentTimeInfo());
 };
